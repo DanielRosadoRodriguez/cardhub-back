d8ca3a3efb11a9396f8347e0cb9e94354765808b
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2023-10-24 21:18:09 -0600

    feat: se ha agregado el método en views que se activa cuando el usuario se conecta a la vista

--- cardhub/views.py
+++ cardhub/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def submit_form(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        name = request.POST['name']
+        password = request.POST['password']
+        
+        print(f"Email: {email}")
+        print(f"Name: {name}")
+        print(f"Password: {password}")
+
+        return HttpResponse("Form submitted successfully!")
+    else:
+        return HttpResponse("Invalid form submission method")
