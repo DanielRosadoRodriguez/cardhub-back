1ce325120b8788b4de7fe1ccfa94281b4acb42ef
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2023-11-03 10:54:19 -0600

    style: moved email pk field first in the user class

--- cardhub/models.py
+++ cardhub/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    name = models.CharField(max_length=100)
     email = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     
     
