5660cbcf7aa79c479e2e06101089764500eff0a4
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2023-11-03 10:44:57 -0600

    style: put user model first

--- cardhub/models.py
+++ cardhub/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class CreditCardProduct(models.Model):
-    card_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    associated_bank = models.CharField(max_length=100)
-    interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
-
-
-class Wallet(models.Model):
-    credit_card_products = models.ManyToManyField(CreditCardProduct)
-
 
 class UserWithWallet(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100, primary_key=True)
     password = models.CharField(max_length=100)
+    
+    
+class CreditCardProduct(models.Model):
+    card_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    associated_bank = models.CharField(max_length=100)
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    
+    
+class Wallet(models.Model):
+    credit_card_products = models.ManyToManyField(CreditCardProduct)
+  
