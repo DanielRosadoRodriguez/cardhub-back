389b2057599d612d7c1c5474ee18eaa0a48547e1
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2023-11-23 14:11:47 -0600

    feat: add dao users and dao super class

---
+++ cardhub/dao/Dao.py
@@ -0,0 +1,15 @@
+class Dao:
+    def get():
+        pass
+    
+    def get_all():
+        pass
+    
+    def save():
+        pass
+
+    def update():
+        pass
+    
+    def delete():
+        pass
---
+++ cardhub/dao/UserDao.py
@@ -0,0 +1,32 @@
+from django.http import JsonResponse
+from cardhub.models import User
+
+class UserDao:
+
+    def get(email: str) -> User:
+        return User.objects.get(email=email)
+
+
+    def get_all() -> list[User]:
+        users = User.objects.all()
+        return users
+
+
+    def save(user: User) -> JsonResponse:
+        user.save()
+        return JsonResponse({'status': 'success'})
+
+
+    def update(user: User, params: dict) -> JsonResponse:
+        user = User(
+            name=params['name'],
+            email=params['email'],
+            password=params['password']
+        )
+        user.save()
+        return JsonResponse({'status': 'success'})
+
+
+    def delete(user: User) -> JsonResponse:
+        user.delete()
+        return JsonResponse({'status': 'success'})
