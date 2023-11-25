b7c288b80dc6e5757b8d10cbbea391b5fcfbc49a
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2023-11-25 13:07:29 -0600

    test: add setup test user for the class testUserDao

--- cardhub/tests/test_dao.py
+++ cardhub/tests/test_dao.py
@@ -5,13 +5,18 @@ from cardhub.models import User
 
 class TestUserDao(TransactionTestCase):
     
-    def test_save_user(self):
-        user: User = User(
+    def setUp(self):
+        self.test_user: User = User(
             name='test',
             email='test@test.com',
             password='testpassword'
         )
+    
+    
+    def test_save_user(self):
+
         expected_response:JsonResponse = JsonResponse({'status': 'success'})
-        response:JsonResponse = UserDao().save(user=user)
+        response:JsonResponse = UserDao().save(user=self.test_user)
         assert response.content == expected_response.content
+
 
