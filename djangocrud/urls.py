6e116c2ed08a46e9305b0d6eefb83ed7478f4e31
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2023-11-25 19:30:36 -0600

    feat: add url to path for the method get last statemetn

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -36,4 +36,5 @@ urlpatterns = [
     path('get_all_user_cards/', views.get_all_user_cards, name='test_get_all_user_cards'),
     path('test_get_last_statement/', views.test_get_last_statement, name='test_get_last_statement'),
     path('create_cardholder_for_user_given_email/', views.create_cardholder_for_user_given_email, name='create_cardholder_for_user_given_email'),
+    path('get_last_statement/', views.get_last_statement, name='test_get_last_statement'),
 ]
