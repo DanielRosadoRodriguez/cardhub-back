739fa3f027d4bc91ab47ed2696e338e54af3952c
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2023-11-27 22:01:30 -0600

    feat: add view get last statement file

---
+++ cardhub/app_views/ViewGetLastStatement.py
@@ -0,0 +1,28 @@
+import json
+
+from ..dao.AccountStatementDao import AccountStatementDao
+from django.http import JsonResponse
+
+
+class GetLastStatement():
+        
+    def __init__(self, request):
+        self.request = request
+        self.data = json.loads(request.body)
+        self.cardholder_card_id = self.data['cardholder_card_id']
+
+
+    def render(self):
+        if self.is_post_method(self):
+            return self.build_response()
+        else:
+            return JsonResponse([self.error_message], safe=False)
+    
+
+    def _is_post_method(self):
+        return self.request.method == "POST"
+    
+
+    def _build_response(self):
+        return AccountStatementDao().get_last_card_statement(self.cardholder_card_id)
+
