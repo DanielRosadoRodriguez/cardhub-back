c92c5c63e95d828b571c1c4f9ab5bbaa54171d5a
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2023-11-27 21:26:18 -0600

    feat: add view sign up

---
+++ cardhub/app_views/ViewSignUp.py
@@ -0,0 +1,30 @@
+import json
+
+from cardhub.dao import CardHolderDao
+from cardhub.dao import UserDao
+from cardhub.models import CardHolder, User
+from django.http import JsonResponse
+
+
+class ViewSignUp():
+    
+    def __init__(self, request):
+        self.data = json.loads(request.body)
+        self.request = request
+        self.error_message = "Invalid form submission method"
+    
+    
+    def render(self):
+        if self.request.method == "POST":
+            return self.build_response()
+        else:
+            return JsonResponse([self.error_message], safe=False)
+        
+            
+    def build_response(self):
+        newUser: User = UserDao().build_user(self.data)
+        UserDao().save(newUser)
+        card_holder = CardHolder(user=newUser)
+        CardHolderDao().save(card_holder)
+        response_data = {"signed": True}
+        return JsonResponse([str(response_data["signed"])], safe=False)
